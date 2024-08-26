9176b2ad558cd993e286988b971eea6d219d645c
Jayesh Bhandarkar <bhandarkarjayesh721@gmail.com>
2024-08-27 00:05:12 +0530

    Create Day0_hello_world.js

---
+++ Day0_hello_world.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+    
+    greeting(parameterVariable);
+}
